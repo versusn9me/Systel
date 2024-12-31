5bb515297000c40a38f4b4ce1aa3fbe3f10ba921
Deni-Ashabov <skillblog.web@gmail.com>
2024-12-31 18:58:40 +0300

    rewrite cors domain

--- constants/corsHeaders.ts
+++ constants/corsHeaders.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:5173',
+    'Access-Control-Allow-Origin': 'https://rostelecom-admin.vercel.app',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   },
