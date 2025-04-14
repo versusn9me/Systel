e078e235f6c4946aa736a8b13586ab5e0b820ff5
Vladislav <vkadsshatrov@gmail.com>
2025-04-14 20:22:06 +0500

    ch data

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'http', hostname: 'localhost' },
-      { protocol: 'https', hostname: 'https://systeltry1.vercel.app' },
+      { protocol: 'https', hostname: 'systel-v4rd.vercel.app' },
     ],
   },
 }
