4543dc8a42362bf1969606137ec546db29d51f6e
Vladislav <vkadsshatrov@gmail.com>
2025-04-14 19:53:08 +0500

    1

--- constants/corsHeaders.ts
+++ constants/corsHeaders.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   headers: {
-    'Access-Control-Allow-Origin': 'https://systel-admin-de21.vercel.app/#/cloth/67fd1abb6ac4329f68654dd3/show',
+    'Access-Control-Allow-Origin': 'https://systel-admin-zy9p.vercel.app',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   },
