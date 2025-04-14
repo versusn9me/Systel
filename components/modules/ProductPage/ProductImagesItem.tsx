30849471e37c21b11bef925d77346a559c1bc481
Vladislav <vkadsshatrov@gmail.com>
2025-04-14 20:37:18 +0500

    fix img

--- components/modules/ProductPage/ProductImagesItem.tsx
+++ components/modules/ProductPage/ProductImagesItem.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import { IProductImagesItemProps } from '@/types/product'
-import useImagePreloader from '@/hooks/useImagePreloader'
-import styles from '@/styles/product/index.module.scss'
+import { IProductImagesItemProps } from '@/types/product';
+import useImagePreloader from '@/hooks/useImagePreloader';
+import styles from '@/styles/product/index.module.scss';
 
 const ProductImagesItem = ({ image, imgSize }: IProductImagesItemProps) => {
-  const { handleLoadingImageComplete, imgSpinner } = useImagePreloader()
+  const { handleLoadingImageComplete, imgSpinner } = useImagePreloader();
 
   return (
     <li
@@ -12,16 +11,20 @@ const ProductImagesItem = ({ image, imgSize }: IProductImagesItemProps) => {
         imgSpinner ? styles.img_loading : ''
       }`}
     >
-      <Image
+      <img
         src={image.url}
         alt={image.desc}
         width={imgSize}
         height={imgSize}
-        className='transition-opacity opacity-0 duration'
+        className={`transition-opacity opacity-0 duration ${
+          !imgSpinner ? 'opacity-100' : ''
+        }`}
         onLoad={handleLoadingImageComplete}
+        loading="lazy"
+        style={{ width: imgSize, height: imgSize, objectFit: 'cover' }}
       />
     </li>
-  )
-}
+  );
+};
 
-export default ProductImagesItem
+export default ProductImagesItem;
