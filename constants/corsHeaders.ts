4cef7da5ec355093ba0db6d6aaac710a9a51e5a3
Vladislav <vkadsshatrov@gmail.com>
2025-04-15 16:03:43 +0500

    ch address sdmin

--- constants/corsHeaders.ts
+++ constants/corsHeaders.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:5173',
+    'Access-Control-Allow-Origin': 'https://systel-admin.vercel.app/#/login',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   },
